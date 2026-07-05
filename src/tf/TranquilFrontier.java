46c70715d65cc7ea30b2393b043672e603718a67
jstation <sophiabeachanel@gmail.com>
2026-07-05 19:22:22 +0800

    Refine

---
+++ src/tf/TranquilFrontier.java
@@ -0,0 +1,5 @@
+package tf;
+
+import mindustry.mod.Mod;
+
+public class TranquilFrontier extends Mod {}
---
+++ src/tf/world/blocks/distribution/FilterDuct.java
@@ -0,0 +1,11 @@
+package tf.world.blocks.distribution;
+
+import mindustry.world.blocks.distribution.*;
+import mindustry.gen.Building;
+
+public class FilterDuct extends Duct {
+    public FilterDuct(String name) {
+        super(name);
+    }
+    public class FilterDuctBuild extends DuctBuild {}
+}
