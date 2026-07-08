3c47d5eecff0e94d1f7d98de97f0d1227d3c05f9
jstation <sophiabeachanel@gmail.com>
2026-07-09 00:24:56 +0800

    cx

--- src/tf/world/blocks/distribution/FilterDuct.java
+++ src/tf/world/blocks/distribution/FilterDuct.java
@@ -2,8 +2,11 @@ package tf.world.blocks.distribution;
 
 import mindustry.world.blocks.distribution.*;
 import mindustry.gen.Building;
+import mindustry.type.*;
 
 public class FilterDuct extends Duct {
+    public ItemSeq AllowedItems;
+
     public FilterDuct(String name) {
         super(name);
     }
