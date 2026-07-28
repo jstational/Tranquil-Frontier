01491fa5263e845eea72fe6578057de50a88b868
jstation <sophiabeachanel@gmail.com>
2026-07-28 21:36:25 +0800

    apple7

--- src/tf/TranquilFrontier.java
+++ src/tf/TranquilFrontier.java
@@ -3,5 +3,5 @@ package tf;
 import mindustry.mod.Mod;
 
 public class TranquilFrontier extends Mod {
-    public String desc = "Build large factories and logistics systems!"
+    public String desc = ""
 }
